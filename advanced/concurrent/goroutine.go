c048a07ad4c6f1af5d6cfa93e155cc5d0ce8b424
Flandre3569 <1332726596@qq.com>
2022-05-17 14:41:57 +0800

    goroutine debug

--- advanced/concurrent/goroutine.go
+++ advanced/concurrent/goroutine.go
@@ -1,18 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func hello(i int) {
 	println("hello goroutine:" + fmt.Sprint(i))
 }
 
 // 并发打印数字0-4，速度更快
+
 func HelloGoRoutine() {
 	for i := 0; i < 5; i++ {
 		go func(j int) {
 			hello(j)
 		}(i)
 	}
+	time.Sleep(time.Second)
 }
 func main() {
 	HelloGoRoutine()
