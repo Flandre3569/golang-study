94aa9cc4d045834f685b93206abcc11252befd16
Flandre3569 <1332726596@qq.com>
2022-05-17 15:16:20 +0800

    goroutine -> sync WaitGroup

--- advanced/concurrent/goroutine.go
+++ advanced/concurrent/goroutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -9,6 +10,7 @@ func hello(i int) {
 	println("hello goroutine:" + fmt.Sprint(i))
 }
 
+// 使用goroutine -> 协程实现高并发操作
 // 并发打印数字0-4，速度更快
 
 func HelloGoRoutine() {
@@ -19,6 +21,22 @@ func HelloGoRoutine() {
 	}
 	time.Sleep(time.Second)
 }
+
+// 优化并发执行策略
+
+func ManyGoWait() {
+	var wg sync.WaitGroup // 实现并发安全操作和协程间的同步
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func(j int) {
+			defer wg.Done()
+			hello(j)
+		}(i)
+	}
+	wg.Wait()
+}
+
 func main() {
-	HelloGoRoutine()
+	//HelloGoRoutine()
+	ManyGoWait()
 }
