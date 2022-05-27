bffce2bb8ebc25e1400659f377698fb14406b660
Flandre3569 <1332726596@qq.com>
2022-05-28 00:00:11 +0800

    update -> CURD

--- gorm-example/easy-start/CURD.go
+++ gorm-example/easy-start/CURD.go
@@ -9,24 +9,34 @@ import (
 // 注意此处实体的key必须是大写开头
 
 type User struct {
+	Id       int
 	Name     string
 	Password string
 }
 
 func main() {
-	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/db2019210745"))
+	// 连接数据库
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/db2019210745?charset=utf8mb4&parseTime=True&loc=Local"))
 
 	if err != nil {
 		fmt.Println("连接数据库失败")
 	}
-	//var users []User
-	//err = db.Select("name", "password").Find(&users, 1).Error
-	//
-	//if err != nil {
-	//	fmt.Println("连接数据库失败")
-	//}
-	//fmt.Println(users)
-	user := User{Name: "小红", Password: "123321"}
-	result := db.Create(&user)
-	fmt.Println(result)
+
+	// 插入一条内容到数据库
+	//user := User{Name: "小红", Password: "123321"}
+	//result := db.Create(&user)
+	// 返回影响数据库的行数
+	//fmt.Println(result.RowsAffected)
+
+	// 查询用户
+	var user User
+	//db.First(&user, 14)
+	//fmt.Println(user)
+
+	// 删除一条信息
+	//db.First(&user, 21).Delete(&user)
+
+	// 修改一条信息
+	db.First(&user, 14).Update("password", "54250")
+
 }
